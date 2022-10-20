04693830c86bf4429732c58b939423bb14303254
MaxX <33282787+m1644@users.noreply.github.com>
2022-10-20 19:04:58 +0300

    Add bot_commands.py

---
+++ bot_commads.py
@@ -0,0 +1,26 @@
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+from spy import *
+
+async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
+    await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
+
+async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
+    await update.message.reply_text(f'{datetime.datetime.now().time()}')
+   
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
+    await update.message.reply_text(f'/hi\n/time\n/help')
+
+async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
+    msg = update.message.text
+    print(msg)
+    items = msg.split() # /sum 123 534543
+    x = int(items[1])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x+y}')
